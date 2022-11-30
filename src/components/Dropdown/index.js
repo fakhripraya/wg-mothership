0bd56c471ef5f166297980004652b3c1d62eb982
fakhripraya <fakhri@indomaret.co.id>
2022-11-30 18:37:30 +0700

    card gap fix

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -16,7 +16,7 @@ export default function Dropdown(props) {
 
     return <div
         className="dropdown-container">
-        <span style={{ display: props.showTitle === false && "none" }} className="dropdown-title">Sort by: </span>
+        <span style={{ display: props.showTitle === false && "none" }} className="dropdown-title">Sort by : </span>
         <div style={props.style} className="dropdown-button-container">
             <button onClick={() => handleOpenFilter()} className="dropdown-button-wrapper">
                 <label className="dropdown-value dropdown-button-label">
