439daacb80cadb9c9fd2814fc5f1171fe0e9639f
Fakhri Prayatna <50196922+fakhripraya@users.noreply.github.com>
2022-12-08 02:37:02 +0700

    NAVBAR V0.2.0 (IMPORTANT CHANGE)

--- src/pages/Remote/index.js
+++ src/pages/Remote/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '../../components/Button';
 import Dropdown from '../../components/Dropdown';
-import Checkbox from '../../components/Checkbox';
+import Checkbox from '../../components/RadioButton';
 import TextInput from '../../components/TextInput';
 import './style.scss';
 import { filterCheckboxes, filterDropdowns, itemListDummy } from '../../variables/dummy/remote';
