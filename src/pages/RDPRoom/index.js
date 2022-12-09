844ffdd3da81664c459247b9141bf76524b3a810
Fakhri Prayatna <50196922+fakhripraya@users.noreply.github.com>
2022-12-10 00:35:57 +0700

    add webrtc

--- src/pages/RDPRoom/index.js
+++ src/pages/RDPRoom/index.js
@@ -1,35 +1,119 @@
-import React from 'react';
+import React, { useRef,useEffect } from 'react';
+import io from "socket.io-client";
+import { iceConfig } from '../../config/rtc/ice';
 import './style.scss';
-import TextInput from '../../components/TextInput';
-import Button from '../../components/Button';
-import { useEffect } from 'react';
 
-export default function RDPRoom() {
+export default function RDPRoom(props) {
+    const userVideo = useRef();
+    const partnerVideo = useRef();
+    const peerRef = useRef();
+    const socketRef = useRef();
+    const otherUser = useRef();
+    const userStream = useRef();
 
     useEffect(() => {
-        // Scroll window to top
-        window.scrollTo(0, 0);
-        // set the overflow to hidden specific for rdp page
-        window.document.getElementsByTagName('html')[0].style.overflow = "hidden";
+        navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
+            userVideo.current.srcObject = stream;
+            userStream.current = stream;
 
-        // return the overflow to the default value
-        return () => window.document.getElementsByTagName('html')[0].style.overflow = "auto";
-    }, [])
+            socketRef.current = io.connect("/");
+            socketRef.current.emit("join room", props.match.params.code);
+
+            socketRef.current.on('other user', userID => {
+                callUser(userID);
+                otherUser.current = userID;
+            });
+
+            socketRef.current.on("user joined", userID => {
+                otherUser.current = userID;
+            });
+
+            socketRef.current.on("offer", handleRecieveCall);
+
+            socketRef.current.on("answer", handleAnswer);
+
+            socketRef.current.on("ice-candidate", handleNewICECandidateMsg);
+        });
+
+    }, []);
+
+    function callUser(userID) {
+        peerRef.current = createPeer(userID);
+        userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
+    }
+
+    function createPeer(userID) {
+        const peer = new RTCPeerConnection(iceConfig.iceServers);
+
+        peer.onicecandidate = handleICECandidateEvent;
+        peer.ontrack = handleTrackEvent;
+        peer.onnegotiationneeded = () => handleNegotiationNeededEvent(userID);
+
+        return peer;
+    }
+
+    function handleNegotiationNeededEvent(userID) {
+        peerRef.current.createOffer().then(offer => {
+            return peerRef.current.setLocalDescription(offer);
+        }).then(() => {
+            const payload = {
+                target: userID,
+                caller: socketRef.current.id,
+                sdp: peerRef.current.localDescription
+            };
+            socketRef.current.emit("offer", payload);
+        }).catch(e => console.log(e));
+    }
+
+    function handleRecieveCall(incoming) {
+        peerRef.current = createPeer();
+        const desc = new RTCSessionDescription(incoming.sdp);
+        peerRef.current.setRemoteDescription(desc).then(() => {
+            userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
+        }).then(() => {
+            return peerRef.current.createAnswer();
+        }).then(answer => {
+            return peerRef.current.setLocalDescription(answer);
+        }).then(() => {
+            const payload = {
+                target: incoming.caller,
+                caller: socketRef.current.id,
+                sdp: peerRef.current.localDescription
+            }
+            socketRef.current.emit("answer", payload);
+        })
+    }
+
+    function handleAnswer(message) {
+        const desc = new RTCSessionDescription(message.sdp);
+        peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
+    }
+
+    function handleICECandidateEvent(e) {
+        if (e.candidate) {
+            const payload = {
+                target: otherUser.current,
+                candidate: e.candidate,
+            }
+            socketRef.current.emit("ice-candidate", payload);
+        }
+    }
+
+    function handleNewICECandidateMsg(incoming) {
+        const candidate = new RTCIceCandidate(incoming);
+
+        peerRef.current.addIceCandidate(candidate)
+            .catch(e => console.log(e));
+    }
+
+    function handleTrackEvent(e) {
+        partnerVideo.current.srcObject = e.streams[0];
+    };
 
     return (
-        <div className="rdp-room-container">
-            <div className="rdp-room-wrapper">
-                <div className="rdp-room-textinput-box">
-                    <h3 className="rdp-room-input-title">Rental Url</h3>
-                    <h4 className="margin-top-0 margin-bottom-12-18">Input the invitation link from your rental host, if you haven't get the URL you can ask the host to <span className="main-color">re-generate</span> the link</h4>
-                    <TextInput type="text" className="rdp-room-textinput text-align-center">
-
-                    </TextInput>
-                    <Button className="rdp-room-button-connect">
-                        Connect
-                    </Button>
-                </div>
-            </div>
+        <div>
+            <video autoPlay ref={userVideo} />
+            <video autoPlay ref={partnerVideo} />
         </div>
-    )
+    );
 }
