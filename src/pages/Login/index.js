26fb420b11c84eb3efd5bd146979ac5745a4e33d
fakhriprayatna.putra <fakhriprayatna.putra@homecredit.co.id>
2023-04-04 22:01:24 +0700

    Update index.js

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -44,6 +44,7 @@ export default function Login(props) {
 
     function handleLoginRequest(callback) {
         console.log(process.env.REACT_APP_OLYMPUS_SERVICE)
+        console.log(process.env.OLYMPUS_SERVICE)
         trackPromise(
             postCredentialService.postData({
                 endpoint: process.env.REACT_APP_OLYMPUS_SERVICE,
