4e1e0de4e8410613ef0bbcc0146a9ccfe8b10bd9
fakhripraya <fakhripraya.p@gmail.com>
2023-12-15 12:52:18 +0700

    Update index.js

--- src/components/Global/index.js
+++ src/components/Global/index.js
@@ -1,26 +1,19 @@
 import WGLogo from "../../assets/svg/LIVEJB_V1_LOGO.svg";
 
-export const ShowNavbar = (props) => {
-  function handleGoToHome() {
-    window.location.href = "/";
-  }
-  // TODO: nanti buat logo yang support background ijo
-  return (
-    <div className="navbar-container">
-      <div className="navbar-wrapper">
-        <div className="navbar-mobile-logo-wrapper">
-          <img
-            onClick={() => handleGoToHome()}
-            className="navbar-logo-img"
-            src={WGLogo}
-            alt="WG_LOGO"
-          />
-        </div>
-        {props.children}
+export const ShowNavbar = (props) => (
+  <div className="navbar-container">
+    <div className="navbar-wrapper">
+      <div className="navbar-mobile-logo-wrapper">
+        <img
+          className="navbar-logo-img"
+          src={WGLogo}
+          alt="WG_LOGO"
+        />
       </div>
+      {props.children}
     </div>
-  );
-};
+  </div>
+);
 
 export const ShowBreadcrumbs = (props) => {
   let breadCrumbTexts = "";
